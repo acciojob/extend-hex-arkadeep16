09b7f0579802e2b01f2a16bb15159c0ba068bbde
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-05-12 21:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const extendHex = (shortHex) => {
   // write your code here
-	forEach((currVal)={
-		console.log(currVal);
-	})
+// Remove the '#' prefix if it exists
+// Expand each component and concatenate them
+// Add the '#' prefix and return the full hex code
+
+	if (shortHex.startsWith('#')) {
+    shortHex = shortHex.slice(1);
+  }
+  // console.log(shortHex);
+  let fullHex = '';
+  for (let i = 0; i < 3; i++) {
+    const element = shortHex[i];
+    // console.log(element);
+    fullHex = fullHex + element + element;
+  }
+  return '#' + fullHex;
+	
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+// const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
